50d9e78e91fb00811f991c4dbb6af4d13213b638
Ankaericyan <79460134+Ankaericyan@users.noreply.github.com>
2022-07-02 20:07:45 +0400

    Create arrayMethod.js

---
+++ arrayMethod.js
@@ -0,0 +1,90 @@
+// let arr = [2,3]
+// let b = arr
+// let arr2 = new Array(2,3)
+// console.log(arr2)
+// console.log(typeof arr)
+// console.log( b=== arr)
+// console.log( arr=== arr2)
+
+// ------------push
+// let array = [1,2,5,6,8]
+// array[3] = "barev"
+// array.push(65, 25) 
+// console.log(array.push())
+
+// ---------------pop
+// let array = [1,2,5,6,8]
+// console.log(array.pop())
+// console.log(array)
+// -----------------
+
+
+// ----------shift
+// let array = [1,2,5,6,8]
+// console.log(array.shift())
+// ------------------
+
+// -------------unshift
+// let array = [1,2,5,6,8]
+// array.unshift(4, 3)
+// console.log(array)
+// ---------------
+
+// -----------splice
+// let array = [1,2,5,6,8]
+// array.splice(2,2 )
+// console.log(array)
+// console.log(array.splice(2,2 ))
+
+// let array = [1,2,5,6,8]
+// array.splice(2,0, 3, 5 )
+// console.log(array.splice(2,1, 3, 5 ))
+
+// ---------------indexOf
+// let array = [1,2,5,6,8,5]
+// console.log(array.indexOf(5))
+
+// -----------------
+
+// -------------lastIndexOf
+// let array = [1,2,5,6,8,5]
+// console.log(array.lastIndexOf(5))
+
+// --------------------includes
+
+// let array = [1,2,5,6,8,5, NaN]
+// console.log(array.includes(NaN))
+
+// 25.06
+// ----------------------------concat
+
+// let arr = [1,5,6,8]
+// console.log(arr.concat(["dnsc"],36,58))
+
+// ------------------------for of
+// let arr = [2,6,8,15,265]
+// for(let item of arr){
+//     console.log(item)
+// }
+
+// ------------)
+// let arr = [2,6,8,15,265]
+// console.log(arr.slice(1,3))
+// console.log(arr);
+// ------------------------
+//  let newarr = "hello".split("")
+// console.log(newarr.join("*"));
+
+// ------------------forEach
+
+// let arr = [2,6,8,15,265]
+// arr.forEach((item, index, array) => {
+//     console.log(item);
+//     console.log(index);
+//     console.log(array);
+// });
+
+// -------------------------filter 
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter(words => words.length > 6);
+// console.log(result);
